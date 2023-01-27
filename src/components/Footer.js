d77436a64416c87a519cba977514c420722fe06e
3mpty <aurelien.corre@outlook.fr>
2023-01-27 23:26:11 +0100

    Footer design done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,10 @@ const Footer = () => {
       <div className="animBorder"></div>
       <footer>
         <h2>Contact</h2>
-        <div className="tel">
-          <a href="tel:+33659920450"></a>
-        </div>
-        <div className="email">
-          <a href="mailto:aurelien.corre@outlook.fr"></a>
-        </div>
+        <div className="tel"></div>
+        <div className="email"></div>
         <div className="adress"></div>
       </footer>
-      ;
     </>
   );
 };
